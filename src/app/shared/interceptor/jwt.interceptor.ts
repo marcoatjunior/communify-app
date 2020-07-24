69eaf84f5f73d28a999afc1ac725f32a4f6a2931
Marco Taborda <marco.taborda@dbserver.com.br>
2020-07-24 13:45:42 -0300

    Adiciona token ao interceptor

--- src/app/shared/interceptor/jwt.interceptor.ts
+++ src/app/shared/interceptor/jwt.interceptor.ts
@@ -6,19 +6,24 @@ import {
   HttpInterceptor,
 } from "@angular/common/http";
 import { Observable } from "rxjs";
+import { User } from "@shared/models/user";
 
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
+  currentUser: User;
+
   constructor() {}
 
   intercept(
     request: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
-    if (localStorage.getItem("token")) {
+    this.currentUser = JSON.parse(localStorage.getItem("currentUser"));
+    const { token } = this.currentUser;
+    if (token) {
       request = request.clone({
         setHeaders: {
-          Authorization: `Bearer ${localStorage.getItem("token")}`
+          Authorization: `Bearer ${token}`,
         },
       });
     }
