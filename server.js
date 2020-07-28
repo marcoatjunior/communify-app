f3e91f22f40d9569fe3a0dfb42463d542713c352
Marco Taborda <marco.taborda@dbserver.com.br>
2020-07-28 11:01:55 -0300

    Teste de requisiçoes https com express-sslify

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
 //Install express server
 const express = require("express");
 const path = require("path");
-const https = require('https');
-const fs = require('fs');
-
-const options = {
-  key: fs.readFileSync('key.pem'),
-  cert: fs.readFileSync('cert.pem')
-};
+const enforce = require('express-sslify');
+const http = require('http');
 
 const app = express();
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + "/dist/communify-app"));
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
 
 app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname + "/dist/communify-app/index.html"));
 });
 
 // Start the app by listening on the default Heroku port
-https.createServer(options, app).listen(process.env.PORT || 8080)
+// app.listen(process.env.PORT || 8080);
+
+http.createServer(app).listen(process.env.PORT || 8080, function() {
+  console.log('Express server listening on port ' + process.env.PORT || 8080);
+});
